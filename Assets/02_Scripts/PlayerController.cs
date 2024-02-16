294708f28b60635524fcd523688918c058b69e8e
JAeUNg <lsam3333333@gmail.com>
2024-02-16 21:45:28 +0900

    플레이어 기본 이동 추가

---
+++ Assets/02_Scripts/PlayerController.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    PlayerInputActions inputActions;
+
+    Rigidbody rigid;
+
+    float moveX = 0;
+    float moveZ = 0;
+    Vector3 movement = Vector3.zero;
+    public float moveSpeed = 2;
+
+    private void Awake()
+    {
+        inputActions = new PlayerInputActions();
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        Move();
+    }
+
+    private void OnEnable()
+    {
+        inputActions.Player.Enable();
+        inputActions.Player.Move.performed += OnMoveInput;
+        inputActions.Player.Move.canceled += OnMoveInput;
+        inputActions.Player.Interact.performed += OnInterctInput;
+    }
+    private void OnDisable()
+    {
+        inputActions.Player.Interact.performed -= OnInterctInput;
+        inputActions.Player.Move.canceled -= OnMoveInput;
+        inputActions.Player.Move.performed -= OnMoveInput;
+        inputActions.Player.Disable();
+    }
+
+    private void OnInterctInput(InputAction.CallbackContext obj)
+    {
+        
+    }
+
+    private void OnMoveInput(InputAction.CallbackContext obj)
+    {
+        SetupInput(obj.ReadValue<Vector2>());
+    }
+
+    void SetupInput(Vector2 input)
+    {
+        moveX = input.x;
+        moveZ = input.y;
+        movement = new Vector3 (moveX, 0, moveZ).normalized;
+    }
+
+    void Move()
+    {
+        rigid.velocity = movement * moveSpeed;
+    }
+
+}
