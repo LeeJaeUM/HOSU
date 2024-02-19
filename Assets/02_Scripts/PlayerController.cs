29eb2111bc2cf5a4ecb7c1e7c1b90678a9c1951c
LeeJaeUng <lsam3333333@gmail.com>
2024-02-19 13:07:37 +0900

    1인칭 테스트 버전 제작

--- Assets/02_Scripts/PlayerController.cs
+++ Assets/02_Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using Cinemachine;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -12,20 +13,39 @@ public class PlayerController : MonoBehaviour
 
     float moveX = 0;
     float moveZ = 0;
+    float mouseX = 0;
+    float mouseY = 0;
     Vector3 movement = Vector3.zero;
     public float moveSpeed = 3;
 
     public Transform MainCameraTr;
+    [Header("Rotate")]
+    public float mouseSpeed = 1;
+    float yRotation;
+    float xRotation;
+    //public Camera cam;
+    public CinemachineVirtualCamera cam; 
 
     private void Awake()
     {
         inputActions = new PlayerInputActions();
-        rigid = GetComponent<Rigidbody>();
+        rigid = GetComponent<Rigidbody>(); 
+        rigid.freezeRotation = true;
+        
+        Cursor.lockState = CursorLockMode.Locked;   // 마우스 커서를 화면 안에서 고정
+        //Cursor.visible = false;                     // 마우스 커서를 보이지 않도록 설정
+
+        //cam = Camera.main;                          // 메인 카메라를 할당
+        cam = GetComponentInChildren<CinemachineVirtualCamera>();// 메인 카메라를 할당
     }
 
     private void FixedUpdate()
     {
         Move();
+    }
+    private void Update()
+    {
+        Rotate();
     }
 
     private void OnEnable()
@@ -34,13 +54,21 @@ public class PlayerController : MonoBehaviour
         inputActions.Player.Move.performed += OnMoveInput;
         inputActions.Player.Move.canceled += OnMoveInput;
         inputActions.Player.Interact.performed += OnInterctInput;
+        inputActions.Player.Look.performed += OnLookInput;
     }
+
     private void OnDisable()
     {
+        inputActions.Player.Look.performed -= OnLookInput;
         inputActions.Player.Interact.performed -= OnInterctInput;
         inputActions.Player.Move.canceled -= OnMoveInput;
         inputActions.Player.Move.performed -= OnMoveInput;
         inputActions.Player.Disable();
+    }
+
+    private void OnLookInput(InputAction.CallbackContext context)
+    {
+        SetupMouseInput(context.ReadValue<Vector2>());
     }
 
     private void OnInterctInput(InputAction.CallbackContext obj)
@@ -57,14 +85,33 @@ public class PlayerController : MonoBehaviour
     {
         moveX = input.x;
         moveZ = input.y;
-        movement = new Vector3 (moveX, 0, moveZ).normalized;
+        movement = transform.right * moveX + transform.forward * moveZ;
     }
 
     void Move()
     {
-        Vector3 cameraForward = 
+        rigid.velocity = movement.normalized * moveSpeed; 
+        //transform.position += movement.normalized * moveSpeed * Time.fixedDeltaTime;
+    }
 
-        rigid.velocity = movement * moveSpeed; 
+    void SetupMouseInput(Vector2 input)
+    {
+        mouseX = input.x;
+        mouseY = input.y;   
+    }
+
+    void Rotate()
+    {
+        float _mouseX = mouseX * mouseSpeed * Time.deltaTime;
+        float _mouseY = mouseY * mouseSpeed * Time.deltaTime;
+
+        yRotation += _mouseX;    // 마우스 X축 입력에 따라 수평 회전 값을 조정
+        xRotation -= _mouseY;    // 마우스 Y축 입력에 따라 수직 회전 값을 조정
+
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);  // 수직 회전 값을 -90도에서 90도 사이로 제한
+
+        cam.transform.rotation = Quaternion.Euler(xRotation, yRotation, 0); // 카메라의 회전을 조절
+        transform.rotation = Quaternion.Euler(0, yRotation, 0);             // 플레이어 캐릭터의 회전을 조절
     }
 
 }
--- Assets/09_Input/PlayerInputActions.cs
+++ Assets/09_Input/PlayerInputActions.cs
@@ -44,6 +44,15 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
                     ""processors"": """",
                     ""interactions"": """",
                     ""initialStateCheck"": false
+                },
+                {
+                    ""name"": ""Look"",
+                    ""type"": ""Value"",
+                    ""id"": ""8963ec43-9513-4270-bf9c-5be1a8423f35"",
+                    ""expectedControlType"": ""Vector2"",
+                    ""processors"": """",
+                    ""interactions"": """",
+                    ""initialStateCheck"": true
                 }
             ],
             ""bindings"": [
@@ -112,6 +121,17 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
                     ""action"": ""Interact"",
                     ""isComposite"": false,
                     ""isPartOfComposite"": false
+                },
+                {
+                    ""name"": """",
+                    ""id"": ""d32a9782-3a52-4688-8fbe-45699efc7351"",
+                    ""path"": ""<Mouse>/delta"",
+                    ""interactions"": """",
+                    ""processors"": """",
+                    ""groups"": """",
+                    ""action"": ""Look"",
+                    ""isComposite"": false,
+                    ""isPartOfComposite"": false
                 }
             ]
         }
@@ -139,6 +159,7 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
         m_Player = asset.FindActionMap("Player", throwIfNotFound: true);
         m_Player_Move = m_Player.FindAction("Move", throwIfNotFound: true);
         m_Player_Interact = m_Player.FindAction("Interact", throwIfNotFound: true);
+        m_Player_Look = m_Player.FindAction("Look", throwIfNotFound: true);
     }
 
     public void Dispose()
@@ -202,12 +223,14 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
     private List<IPlayerActions> m_PlayerActionsCallbackInterfaces = new List<IPlayerActions>();
     private readonly InputAction m_Player_Move;
     private readonly InputAction m_Player_Interact;
+    private readonly InputAction m_Player_Look;
     public struct PlayerActions
     {
         private @PlayerInputActions m_Wrapper;
         public PlayerActions(@PlayerInputActions wrapper) { m_Wrapper = wrapper; }
         public InputAction @Move => m_Wrapper.m_Player_Move;
         public InputAction @Interact => m_Wrapper.m_Player_Interact;
+        public InputAction @Look => m_Wrapper.m_Player_Look;
         public InputActionMap Get() { return m_Wrapper.m_Player; }
         public void Enable() { Get().Enable(); }
         public void Disable() { Get().Disable(); }
@@ -223,6 +246,9 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
             @Interact.started += instance.OnInteract;
             @Interact.performed += instance.OnInteract;
             @Interact.canceled += instance.OnInteract;
+            @Look.started += instance.OnLook;
+            @Look.performed += instance.OnLook;
+            @Look.canceled += instance.OnLook;
         }
 
         private void UnregisterCallbacks(IPlayerActions instance)
@@ -233,6 +259,9 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
             @Interact.started -= instance.OnInteract;
             @Interact.performed -= instance.OnInteract;
             @Interact.canceled -= instance.OnInteract;
+            @Look.started -= instance.OnLook;
+            @Look.performed -= instance.OnLook;
+            @Look.canceled -= instance.OnLook;
         }
 
         public void RemoveCallbacks(IPlayerActions instance)
@@ -263,5 +292,6 @@ public partial class @PlayerInputActions: IInputActionCollection2, IDisposable
     {
         void OnMove(InputAction.CallbackContext context);
         void OnInteract(InputAction.CallbackContext context);
+        void OnLook(InputAction.CallbackContext context);
     }
 }
